e1d10b0bd1e6efc96019ba00f525c4ea2a9433d3
Dolimov Khumoyunmirzo| <humodalim@gmail.com>
2019-07-05 09:17:19 +0500

    Task finished

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let count = 0;
+  for (let i = 1; i <= preferences.length; i++) {
+    if (i === preferences[preferences[preferences[i - 1] - 1] - 1]) {
+      count++;
+    }
+  }
+  return Math.floor(count / 3);
 };
